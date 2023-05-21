f29badc09dd51fc03ad73837d6d7ed7ac71ce4ab
ParkerSnow <parkersnow40@yahoo.com>
2023-05-20 18:46:24 -0600

    Changed to a nearest neighbors

--- Code/ModelGeneration.py
+++ Code/ModelGeneration.py
@@ -6,7 +6,7 @@ import pandas as pd
 data = pd.read_csv('Data/data.csv',index_col='id')
 #print(data)
 
-model = impute.SimpleImputer(missing_values=np.NAN, strategy='mean')
+model = impute.KNNImputer(missing_values=np.NAN)
 
 trainData = data[['pressure [MPa]','mass_flux [kg/m2-s]','x_e_out [-]']]
 
@@ -15,14 +15,20 @@ trainData = data[['pressure [MPa]','mass_flux [kg/m2-s]','x_e_out [-]']]
 model.fit(trainData)
 model.set_output(transform='pandas')
 finalData = model.transform(trainData)
-print(finalData)
+
 sample = pd.read_csv('Data/sample_submission.csv')
 
+values = []
+ids = []
+
 for i in sample['id']:
-    #print(i)
-    row = finalData.iloc(i)
-    print(row['mass_flux [kg/m2-s]'])
-sumbission = pd.DataFrame(index=sample['id'])
+    value = finalData.iloc[i]['x_e_out [-]']
 
+    values.append(value)
+    ids.append(i)
 
+submission = pd.DataFrame(index=ids,data=values,columns=['x_e_out [-]'])
 
+print(submission)
+
+submission.to_csv('Data/submission.csv',index_label='id')
